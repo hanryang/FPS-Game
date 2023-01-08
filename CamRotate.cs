d21097b66b5866a45df1709983ec69dfa62c3489
hanryang <102446368+hanryang@users.noreply.github.com>
2023-01-08 10:29:03 +0900

    Update CamRotate.cs
    Where to rotate

--- CamRotate.cs
+++ CamRotate.cs
@@ -12,5 +12,8 @@ public class CamRotate : MonoBehaviour
         // 1. 마우스 입력을 받는다.
         float mouse_X = Input.GetAxis("Mouse X");
         float mouse_Y = Input.GetAxis("Mouse Y");
+
+        // 2. 마우스 입력 값을 이용해 회전 방향을 결정한다.
+        Vector3 dir = new Vector3(-mouse_Y, mouse_X, 0);
     }
 }
