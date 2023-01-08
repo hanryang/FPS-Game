4e4cd5bdf3bb9b3e38563ec17e01fa1c23550191
hanryang <102446368+hanryang@users.noreply.github.com>
2023-01-08 10:42:53 +0900

    Update CamRotate.cs
    fix the problem when debug in unity

--- CamRotate.cs
+++ CamRotate.cs
@@ -7,6 +7,10 @@ public class CamRotate : MonoBehaviour
     // 회전 속도 변수
     public float rotSpeed = 200f;
 
+    // 회전 값 변수
+    float mx = 0;
+    float my = 0;
+
     // Update is called once per frame
     void Update()
     {
@@ -15,8 +19,15 @@ public class CamRotate : MonoBehaviour
         float mouse_X = Input.GetAxis("Mouse X");
         float mouse_Y = Input.GetAxis("Mouse Y");
 
+        // 1-1 회전 값 변수에 마우스 입력 값만큼 미리 누적시킨다.
+        mx += mouse_X * rotSpeed * Time.deltaTime;
+        my += mouse_Y * rotSpeed * Time.deltaTime;
+
+        // 1-2 마우스 상하 이동 회전 변수(my)의 값을 -90도 ~ 90도 사이로 제한한다.
+        my = Mathf.Clamp(my, -90f,  90f);
+
         // 2. 마우스 입력 값을 이용해 회전 방향을 결정한다.
-        Vector3 dir = new Vector3(-mouse_Y, mouse_X, 0);
+        Vector3 dir = new Vector3(-my, mx, 0);
 
         // 3. 회전 방향으로 물체를 회전시킨다.
         // r = r0 + vt
